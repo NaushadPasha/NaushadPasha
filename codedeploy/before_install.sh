848c79d2b85c13790ef7510bfa195ef9141186d1
NaushadPasha <33582239+NaushadPasha@users.noreply.github.com>
2021-10-08 14:51:17 -0400

    Update before_install.sh

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -7,8 +7,8 @@ yum -y update
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install nodejs
 
-npm install -g pm2
-pm2 update
+#npm install -g pm2
+# update
 
 export app_root=/usr/cddemo
 if [ -d "$app_root" ];then
